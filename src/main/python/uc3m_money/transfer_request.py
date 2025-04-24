a009539454b9e9f939c6dea9cf3c7910db804030
ethanbsung <ebsung@umich.edu>
2025-04-24 21:38:23 +0200

    transfer_request tests passing again

--- src/main/python/uc3m_money/transfer_request.py
+++ src/main/python/uc3m_money/transfer_request.py
@@ -1,5 +1,6 @@
 """Transfer request module"""
 import hashlib
+import json
 from datetime import datetime, timezone
 
 
@@ -8,69 +9,93 @@ class TransferRequest:
     Represents a request to transfer money between two IBAN accounts.
     Stores key transfer details, timestamp, and supports serialization.
     """
-    def __init__(self, from_iban: str, to_iban: str, transfer_concept: str,
-                 transfer_type: str, transfer_date: str, transfer_amount: float):
+    def __init__(self, from_iban: str, transfer_type: str, to_iban: str,
+                 transfer_concept: str, transfer_date: str, transfer_amount: float):
         self.__from_iban = from_iban
         self.__to_iban = to_iban
-        self.__transfer_concept = transfer_concept
         self.__transfer_type = transfer_type
+        self.__concept = transfer_concept
         self.__transfer_date = transfer_date
         self.__transfer_amount = transfer_amount
-        self.__time_stamp = datetime.timestamp(datetime.now(timezone.utc))
+        justnow = datetime.now(timezone.utc)
+        self.__time_stamp = datetime.timestamp(justnow)
+
+    def __str__(self) -> str:
+        return "Transfer:" + json.dumps(self.__dict__)
 
     @property
     def from_iban(self):
+        """Sender's iban"""
         return self.__from_iban
+
+    @from_iban.setter
+    def from_iban(self, value):
+        self.__from_iban = value
 
     @property
     def to_iban(self):
+        """receiver's iban"""
         return self.__to_iban
+
+    @to_iban.setter
+    def to_iban(self, value):
+        self.__to_iban = value
 
     @property
     def transfer_concept(self):
-        return self.__transfer_concept
+        """Property representing the transfer concept"""
+        return self.__concept
+
+    @transfer_concept.setter
+    def transfer_concept(self, value):
+        self.__concept = value
 
     @property
     def transfer_type(self):
+        """Property representing the type of transfer: REGULAR, INMEDIATE or URGENT"""
         return self.__transfer_type
+
+    @transfer_type.setter
+    def transfer_type(self, value):
+        self.__transfer_type = value
 
     @property
     def transfer_date(self):
+        """Property representing the transfer's date"""
         return self.__transfer_date
+
+    @transfer_date.setter
+    def transfer_date(self, value):
+        self.__transfer_date = value
 
     @property
     def transfer_amount(self):
+        """Property representing the transfer amount"""
         return self.__transfer_amount
+
+    @transfer_amount.setter
+    def transfer_amount(self, value):
+        self.__transfer_amount = value
 
     @property
     def time_stamp(self):
+        """Read-only property that returns the timestamp of the request"""
         return self.__time_stamp
 
     @property
     def transfer_code(self) -> str:
-        """
-        Generates a unique transfer code using a hash of the IBANs, timestamp, and amount.
-        """
-        transfer_string = f"{self.from_iban}{self.to_iban}{self.time_stamp}{self.transfer_amount}"
-        return hashlib.md5(transfer_string.encode()).hexdigest()
+        """Returns the md5 signature (transfer code)"""
+        return hashlib.md5(str(self).encode()).hexdigest()
 
-    def to_json(self) -> dict:
-        """
-        Serializes the transfer request into a dictionary suitable for JSON storage.
-        """
+    def to_json(self):
+        """returns the object information in json format"""
         return {
-            "from_iban": self.from_iban,
-            "to_iban": self.to_iban,
-            "transfer_concept": self.transfer_concept,
-            "transfer_type": self.transfer_type,
-            "transfer_date": self.transfer_date,
-            "transfer_amount": self.transfer_amount,
-            "time_stamp": self.time_stamp,
+            "from_iban": self.__from_iban,
+            "to_iban": self.__to_iban,
+            "transfer_type": self.__transfer_type,
+            "transfer_amount": self.__transfer_amount,
+            "transfer_concept": self.__concept,
+            "transfer_date": self.__transfer_date,
+            "time_stamp": self.__time_stamp,
             "transfer_code": self.transfer_code
         }
-
-    def __str__(self) -> str:
-        return (
-            f"Transfer from {self.from_iban} to {self.to_iban} of {self.transfer_amount} "
-            f"on {self.transfer_date} [{self.transfer_type}] - Concept: {self.transfer_concept}"
-        )
