82b6fb507da32b52633ad79c45575838eaa76986
tejgthomasuc3m <100559032@alumnos.uc3m.es>
2025-04-24 18:04:37 +0200

    update

--- src/main/python/uc3m_money/account_deposit.py
+++ src/main/python/uc3m_money/account_deposit.py
@@ -1,62 +1,57 @@
-"""Contains the class OrderShipping"""
+"""Account deposit module"""
 from datetime import datetime, timezone
 import hashlib
 
-class AccountDeposit():
-    """Class representing the information required for shipping of an order"""
 
-    def __init__(self,
-                 to_iban: str,
-                 deposit_amount: float):
+class AccountDeposit:
+    """
+    Class representing a deposit into an account.
+    Generates a secure deposit signature and provides serialization.
+    """
+    def __init__(self, to_iban: str, deposit_amount: float):
         self.__alg = "SHA-256"
         self.__type = "DEPOSIT"
         self.__to_iban = to_iban
         self.__deposit_amount = deposit_amount
-        justnow = datetime.now(timezone.utc)
-        self.__deposit_date = datetime.timestamp(justnow)
-
-    def to_json(self):
-        """returns the object data in json format"""
-        return {"alg": self.__alg,
-                "type": self.__type,
-                "to_iban": self.__to_iban,
-                "deposit_amount": self.__deposit_amount,
-                "deposit_date": self.__deposit_date,
-                "deposit_signature": self.deposit_signature}
-
-    def __signature_string(self):
-        """Composes the string to be used for generating the key for the date"""
-        return "{alg:" + str(self.__alg) +",typ:" + str(self.__type) +",iban:" + \
-               str(self.__to_iban) + ",amount:" + str(self.__deposit_amount) + \
-               ",deposit_date:" + str(self.__deposit_date) + "}"
+        self.__deposit_date = datetime.timestamp(datetime.now(timezone.utc))
 
     @property
-    def to_iban(self):
-        """Property that represents the product_id of the patient"""
+    def alg(self) -> str:
+        return self.__alg
+
+    @property
+    def type(self) -> str:
+        return self.__type
+
+    @property
+    def to_iban(self) -> str:
         return self.__to_iban
 
-    @to_iban.setter
-    def to_iban(self, value):
-        self.__to_iban = value
-
     @property
-    def deposit_amount(self):
-        """Property that represents the order_id"""
+    def deposit_amount(self) -> float:
         return self.__deposit_amount
-    @deposit_amount.setter
-    def deposit_amount(self, value):
-        self.__deposit_amount = value
 
     @property
-    def deposit_date(self):
-        """Property that represents the phone number of the client"""
+    def deposit_date(self) -> float:
         return self.__deposit_date
-    @deposit_date.setter
-    def deposit_date( self, value ):
-        self.__deposit_date = value
-
 
     @property
-    def deposit_signature( self ):
-        """Returns the sha256 signature of the date"""
-        return hashlib.sha256(self.__signature_string().encode()).hexdigest()
+    def deposit_signature(self) -> str:
+        """
+        Generates a SHA-256 hash to uniquely identify the deposit.
+        """
+        signature_string = f"{self.to_iban}{self.deposit_amount}{self.deposit_date}"
+        return hashlib.sha256(signature_string.encode()).hexdigest()
+
+    def to_json(self) -> dict:
+        """
+        Serializes the deposit into a dictionary suitable for JSON storage.
+        """
+        return {
+            "alg": self.alg,
+            "type": self.type,
+            "to_iban": self.to_iban,
+            "deposit_amount": self.deposit_amount,
+            "deposit_date": self.deposit_date,
+            "deposit_signature": self.deposit_signature
+        }
